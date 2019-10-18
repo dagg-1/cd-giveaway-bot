754fbc1edc349f5561e8e703e5efced44ad6b6b1
Dagg <daggintosh@outlook.com>
2019-10-18 00:01:40 -0700

    The embed actually works

--- bot.js
+++ bot.js
@@ -21,40 +21,51 @@ client.on('message', msg => {
     const command = arguments.shift();
     switch (command) {
         case "create":
-            var count = arguments[0];
-            var convcount = parseInt(count, 36);
+            var count = parseInt(arguments[0]);
             
             // Written by Nyx <3 and Dagg :DogeWtf: 🎉
 
             // Parse data
-            gameTitles = arguments.slice(1, convcount+1);
-            giveawayTime = arguments[convcount + 1];
-            channelId = arguments[convcount + 2];
-            gameKeys = arguments.slice(arguments.length-convcount, (arguments.length))
+            gameTitles = arguments.slice(1, count+1);
+            giveawayTime = arguments[count + 1];
+            channelId = arguments[count + 2];
+            gameKeys = arguments.slice(arguments.length-count, (arguments.length))
 
             let sepTime = giveawayTime.split(/(\d+)/);
+            let date = new Date()
+
             
-            switch (sepTime[3]) {
-                case "m":
-                    break
-                case "h":
-                    break
-                case "d":
-                    break
-                case "m":
-                    break
-            }
 
             let CreEmbed = new Discord.RichEmbed()
             .addField("Games", count)
-            .addField("Time", giveawayTime)
             .addField("Channel", channelId)
             .setColor(Math.floor(Math.random() * 16777215))
             .setFooter(new Date().toUTCString())
+            .setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL)
+
+            switch (sepTime[2]) {
+                case "m":
+                    date.setUTCMinutes(date.getUTCMinutes() + parseInt(sepTime[1]))
+                    break
+                case "h":
+                    date.setUTCHours(date.getUTCHours() + parseInt(sepTime[1]))
+                    break
+                case "d":
+                    date.setUTCDate(date.getUTCDate() + parseInt(sepTime[1]))
+                    break
+                case "w":
+                    date.setUTCDate(date.getUTCDate() + (7 * parseInt(sepTime[1])))
+                    break
+                case "y":
+                    date.setUTCFullYear(date.getUTCFullYear() + parseInt(sepTime[1]))
+                    break
+            }
+            CreEmbed.addField("End Date", date.toUTCString())
 
             var i = 1;
             gameTitles.forEach(element => {
-                CreEmbed.addField(`Game #${i}`, element);
+                let split = element.replace(/-/gi, " ")
+                CreEmbed.addField(`Game #${i}`, split);
                 ++i;
             })
             i = 1;
