c1bae780191b34ad8948d2518826988f8a513afb
Dagg <daggintosh@outlook.com>
2019-10-18 21:25:58 -0700

    Formatting, reaction control

--- bot.js
+++ bot.js
@@ -10,74 +10,110 @@ var channelId = "";
 var gameKeys = [];
 
 client.on('ready', () => {
-    console.log(`Logged in successfully as ${client.user.username}#${client.user.discriminator}`);
+    console.log(`Logged in successfully as ${client.user.tag}`);
 });
 
 // !create [number of items] [name] [time] [channel] [key] 
 
 client.on('message', msg => {
-    console.log(msg.content);
+
+    const filter = (reaction, user) => {
+        return ['🎉', '🚫', '✅'].includes(reaction.emoji.name) && user.id === msg.author.id;
+    };
+
+    console.log(`[${new Date().toLocaleTimeString()}] ${msg.author.tag}: ${msg.content}`);
     const arguments = msg.content.slice(prefix.length).trim().split(" ");
     const command = arguments.shift();
     switch (command) {
         case "create":
             var count = parseInt(arguments[0]);
-            
-            // Written by Nyx <3 and Dagg :DogeWtf: 🎉
+
+            // Written by Nyx <3 and Dagg <:DogeWtf:586419821986578433> 🎉
 
             // Parse data
-            gameTitles = arguments.slice(1, count+1);
+            gameTitles = arguments.slice(1, count + 1);
             giveawayTime = arguments[count + 1];
             channelId = arguments[count + 2];
-            gameKeys = arguments.slice(arguments.length-count, (arguments.length))
+            gameKeys = arguments.slice(arguments.length - count, (arguments.length));
 
+            // Get a new date and place the giveawayTime variable into an array to be examined later
             let sepTime = giveawayTime.split(/(\d+)/);
-            let date = new Date()
+            let endDate = new Date();
+            let currDate = new Date();
 
-            
-
+            // Assemble the embed
             let CreEmbed = new Discord.RichEmbed()
-            .addField("Games", count)
-            .addField("Channel", channelId)
-            .setColor(Math.floor(Math.random() * 16777215))
-            .setFooter(new Date().toUTCString())
-            .setAuthor(`${msg.author.username}#${msg.author.discriminator}`, msg.author.avatarURL)
+                .addField("Games", count)
+                .addField("Channel", channelId)
+                .setColor(Math.floor(Math.random() * 16777215))
+                .setFooter(new Date().toUTCString())
+                .setAuthor(`${msg.author.tag}`, msg.author.avatarURL);
 
+            // Determine the end date
             switch (sepTime[2]) {
                 case "m":
-                    date.setUTCMinutes(date.getUTCMinutes() + parseInt(sepTime[1]))
+                    endDate.setUTCMinutes(endDate.getUTCMinutes() + parseInt(sepTime[1]));
                     break
                 case "h":
-                    date.setUTCHours(date.getUTCHours() + parseInt(sepTime[1]))
+                    endDate.setUTCHours(endDate.getUTCHours() + parseInt(sepTime[1]));
                     break
                 case "d":
-                    date.setUTCDate(date.getUTCDate() + parseInt(sepTime[1]))
+                    endDate.setUTCDate(endDate.getUTCDate() + parseInt(sepTime[1]));
                     break
                 case "w":
-                    date.setUTCDate(date.getUTCDate() + (7 * parseInt(sepTime[1])))
+                    endDate.setUTCDate(endDate.getUTCDate() + (7 * parseInt(sepTime[1])));
                     break
                 case "y":
-                    date.setUTCFullYear(date.getUTCFullYear() + parseInt(sepTime[1]))
+                    endDate.setUTCFullYear(endDate.getUTCFullYear() + parseInt(sepTime[1]));
                     break
-            }
-            CreEmbed.addField("End Date", date.toUTCString())
+            };
+            CreEmbed.addField("End Date", endDate.toUTCString());
 
+            // Add the games to the embed fields
             var i = 1;
             gameTitles.forEach(element => {
-                let split = element.replace(/-/gi, " ")
-                CreEmbed.addField(`Game #${i}`, split);
+                let split = element.replace(/-/gi, " ");
+                CreEmbed.addField(split, "IF THIS IS BLANK, THERE HAS BEEN A FATAL ERROR");
                 ++i;
-            })
+            });
             i = 1;
             gameKeys.forEach(element => {
-                CreEmbed.addField(`Game #${i} Key`, element);
+                CreEmbed.fields[i + 2] = { name: CreEmbed.fields[i + 2].name, value: element, inline: false };
                 ++i;
-            })
+            });
 
-
+            // Post the message
             msg.channel.send(CreEmbed)
+                .then(async newmsg => {
+                    newmsg.createReactionCollector(filter, { time: 120000 })
+                        .on('collect', reaction => {
+                            switch (reaction.emoji.name) {
+
+                            }
+                        })
+                    await newmsg.react("✅")
+                    await newmsg.react("🚫")
+                }); // TODO: Add reaction control for finalisation
             break
     }
 });
 
-client.login(token_file.discord.bot_token)
+client.login(token_file.discord.bot_token);
+
+/*
+let usermap = reaction.users.entries()
+usermap.next()
+console.log(`I collected a reaction, it's name is ${reaction.emoji.name} and it was from ${usermap.next().value[1].tag}`)
+
+let aoNow = new Date()
+                console.log(`END DATE: ${endDate} \n CURRENT DATE: ${aoNow}`)
+                let thisInterval = setInterval(() => {
+                    aoNow = new Date()
+                    console.log(`END DATE: ${endDate} \n CURRENT DATE: ${aoNow}`)
+                    if (endDate <= aoNow) {
+                        newmsg.edit(Math.floor(Math.random() * 100))
+                        clearInterval(thisInterval)
+                    }
+                }, 60000) // TODO: Update the final giveaway message (not this one) every minute for hours remaining
+                // 60000ms should make it repeat every minute at the minimum time
+*/
